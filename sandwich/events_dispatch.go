afd7df2ce7f410e9b12b7f92de4027a71bcc3555
Blane Thompson <rock@welcomer.gg>
2026-01-17 23:01:58 +0000

    increased channel size

--- sandwich/events_dispatch.go
+++ sandwich/events_dispatch.go
@@ -203,7 +203,7 @@ func (h *Handlers) getWorkerPool(eventCtx *EventContext, shardID int32) chan Wor
 		return channel
 	}
 
-	channel = make(chan WorkerMessage)
+	channel = make(chan WorkerMessage, 1024000)
 
 	h.WorkerPool[shardID] = channel
 	go h.worker(eventCtx.Logger, shardID, channel)
